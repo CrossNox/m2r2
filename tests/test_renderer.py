eae4be4c7cfbca42c8153d426d436f26148eb462
Christian Häggström <christian.haggstrom@orexplore.com>
2021-01-25 16:26:22 +0100

    Switch to single-dollar inline math markup

--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -264,14 +264,22 @@ class TestInlineMarkdown(RendererTestBase):
         self.assertEqual(out, '\na ``code`` and `RefLink <a>`_ here.\n')
 
     def test_inline_math(self):
-        src = 'this is `$E = mc^2$` inline math.'
+        src = 'this is $E = mc^2$ inline math.'
         out = self.conv(src)
         self.assertEqual(out, '\nthis is :math:`E = mc^2` inline math.\n')
 
+        self.assertEqual(self.conv('$ee$'), '\n:math:`ee`\n')
+        self.assertEqual(self.conv('$e$'), '\n:math:`e`\n')
+
+    def test_inline_math_money(self):
+        src = 'Thus, $20,000 and $30,000 won’t parse as math'
+        out = self.conv(src)
+        self.assertEqual(out, '\nThus, $20,000 and $30,000 won’t parse as math\n')
+
     def test_disable_inline_math(self):
-        src = 'this is `$E = mc^2$` inline math.'
+        src = 'this is $E = mc^2$ inline math.'
         out = self.conv(src, disable_inline_math=True)
-        self.assertEqual(out, '\nthis is ``$E = mc^2$`` inline math.\n')
+        self.assertEqual(out, '\nthis is $E = mc^2$ inline math.\n')
 
     def test_inline_html(self):
         src = 'this is <s>html</s>.'
