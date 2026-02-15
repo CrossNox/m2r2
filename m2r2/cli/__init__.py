45365fb9137531a956b139f86a232c4699a8489d
CrossNox <ijmermet@gmail.com>
2026-02-14 22:00:37 -0300

    Use __all__ for cli exports instead of redundant aliases

--- m2r2/cli/__init__.py
+++ m2r2/cli/__init__.py
@@ -1,4 +1,5 @@
 """M2R2 command-line interface."""
 
-from m2r2.cli.m2r2 import convert_file as convert_file
-from m2r2.cli.m2r2 import main as main
+from m2r2.cli.m2r2 import convert_file, main
+
+__all__ = ["convert_file", "main"]
