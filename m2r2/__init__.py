fbf2d1ab074273b8d5438d4076bb219f12e02a99
CrossNox <ijmermet@gmail.com>
2026-02-15 22:06:06 -0300

    Remove misleading circular import comment from __init__.py

--- m2r2/__init__.py
+++ m2r2/__init__.py
@@ -4,8 +4,6 @@ from .m2r2 import M2R2, convert
 
 __version__ = version(__name__)
 
-# Imported after __version__ is set to avoid circular import:
-# sphinx.m2r2 -> m2r2.__version__ must already exist.
 from .sphinx.m2r2 import setup
 
 __all__ = ("M2R2", "convert", "setup")
