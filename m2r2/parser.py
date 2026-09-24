3a82f764e2a414c969b937fe8097390d93de002b
CrossNox <ijmermet@gmail.com>
2026-09-24 00:32:59 -0300

    Explain Sphinx conversion in the Markdown parser

--- m2r2/parser.py
+++ m2r2/parser.py
@@ -9,4 +9,6 @@ class M2R2Parser(rst.Parser):
 
     def parse(self, inputstring, document):
         """Parse `inputstring` and populate `document`, a document tree."""
+        # Sphinx calls this Docutils parser for .md files. Its document supplies
+        # the configuration and source path needed by SphinxM2R2.
         super().parse(SphinxM2R2(document).parse(inputstring), document)
